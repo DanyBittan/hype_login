fb53c6f15a5f949f9462c5c147f562ea7248cd11
DanyBittan <danielbittang@gmail.com>
2025-11-08 16:37:13 +0100

    remove of files

--- includes/db_connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'config.php';
-$dbConnection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-$usersData = $dbConnection->query("SELECT * FROM users");
-if ($dbConnection->connect_error) {
-    die("Connection failed: " . $dbConnection->connect_error);
-}
-print_r($usersData);
